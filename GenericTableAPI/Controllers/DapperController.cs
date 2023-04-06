4988a343c54fb3c25150a6b39882ad02c338ffd2
Patryk Nowak <wrx71255@student.wsb.wroclaw.pl>
2023-04-06 10:26:09 +0000

    Merged in logger-implementation (pull request #4)
    Approved-by: Maciej Zagozda

--- GenericTableAPI/Controllers/DapperController.cs
+++ GenericTableAPI/Controllers/DapperController.cs
@@ -1,5 +1,7 @@
 ﻿using GenericTableAPI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Web;
 
 namespace GenericTableAPI.Controllers
 {
@@ -9,47 +11,177 @@ namespace GenericTableAPI.Controllers
     {
         //private readonly DapperRepository _repository;
         private readonly DapperService _service;
+        private readonly ILogger<DapperController> _logger;
 
-        public DapperController(DapperService service)
+        public DapperController(DapperService service, ILogger<DapperController> logger)
         {
             _service = service;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<dynamic>>> GetAll(string tableName)
         {
-            var entities = await _service.GetAllAsync(tableName);
-            return Ok(entities);
+            var timestamp = DateTimeOffset.UtcNow;
+            var requestInfo = $"GET request to {HttpContext.Request.Path}{HttpContext.Request.QueryString} from {HttpContext.Connection.RemoteIpAddress} by user {User.Identity.Name ?? "unknown"}. Timestamp: {timestamp}";
+            _logger.LogInformation(requestInfo);
+            try
+            {
+                _logger.LogInformation("Getting all entities from {tableName}. Timestamp: {timestamp}", tableName, timestamp);
+                var entities = await _service.GetAllAsync(tableName);
+
+                if (!entities.Any())
+                {
+                    _logger.LogInformation("No entities found for {tableName}. Timestamp: {timestamp}", tableName, timestamp);
+                    return NoContent();
+                }
+
+                _logger.LogInformation("Found {count} entities from {tableName}. Timestamp: {timestamp}", entities.Count(), tableName, timestamp);
+                return Ok(entities);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while getting entities from {tableName}. Timestamp: {timestamp}", tableName, timestamp);
+                return StatusCode(500, "An error occurred while processing your request");
+            }
+            finally
+            {
+                var responseInfo = $"Response returned from {HttpContext.Request.Path}{HttpContext.Request.QueryString} with status code {Response.StatusCode}. Timestamp: {timestamp}";
+                _logger.LogInformation(responseInfo);
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<dynamic>> GetById(string tableName, [FromRoute] string id)
         {
-            dynamic? entity = await _service.GetByIdAsync(tableName, id);
+            var timestamp = DateTimeOffset.UtcNow;
+            var requestInfo = $"GET request to {HttpContext.Request.Path}{HttpContext.Request.QueryString} from {HttpContext.Connection.RemoteIpAddress} by user {User.Identity.Name ?? "unknown"}. Timestamp: {timestamp}";
+            _logger.LogInformation(requestInfo);
+            _logger.LogInformation("Getting entity with id \"{id}\" from \"{tableName}\". Timestamp: {timestamp}", id, tableName, timestamp);
 
-            return entity == null ? NotFound() : (ActionResult<dynamic>)Ok(entity);
+            dynamic? entity = null;
+            try
+            {
+                entity = await _service.GetByIdAsync(tableName, id);
+
+                if (entity == null)
+                {
+                    _logger.LogInformation($"No entity found with id \"{id}\" in \"{tableName}\". Request: {requestInfo}");
+                    return NotFound();
+                }
+
+                _logger.LogInformation("Found entity with id \"{id}\" in \"{tableName}\". Timestamp: {timestamp}", id, tableName, timestamp);
+                return Ok(entity);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error while processing request: {requestInfo}. Timestamp: {timestamp}");
+                return StatusCode(500, "An error occurred while processing your request");
+            }
+            finally
+            {
+                var responseInfo = $"Response returned from {HttpContext.Request.Path}{HttpContext.Request.QueryString} with status code {Response.StatusCode}. Timestamp: {timestamp}";
+                _logger.LogInformation(responseInfo);
+            }
         }
 
         [HttpPost]
         public async Task<ActionResult> Add(string tableName, [FromBody] IDictionary<string, object> values)
         {
-            var id = await _service.AddAsync(tableName, values);
-            var newItem = await _service.GetByIdAsync(tableName, id.ToString());
-            return Ok(newItem);
+            var timestamp = DateTimeOffset.UtcNow;
+            var valuesDict = values.ToDictionary(pair => pair.Key, pair => pair.Value?.ToString());
+
+            var requestInfo = $"POST request to \"{HttpContext.Request.Path}\" from \"{HttpContext.Connection.RemoteIpAddress}\" by user \"{User.Identity.Name ?? "unknown"}\" with values: {JsonConvert.SerializeObject(valuesDict)}. Timestamp: {timestamp}";
+            _logger.LogInformation(requestInfo);
+
+            try
+            {
+                _logger.LogInformation("Adding a new entity to \"{tableName}\": {values}. Timestamp: {timestamp}", tableName, JsonConvert.SerializeObject(valuesDict), timestamp);
+                var id = await _service.AddAsync(tableName, values);
+
+                if (id == null)
+                {
+                    _logger.LogInformation("Failed to add a new entity to \"{tableName}\". Timestamp: {timestamp}", tableName, timestamp);
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                }
+
+                var newItem = await _service.GetByIdAsync(tableName, id.ToString());
+
+                _logger.LogInformation("Added a new entity with id \"{id}\" to \"{tableName}\". Timestamp: {timestamp}", id, tableName, timestamp);
+                return Ok(newItem);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error while processing request: {requestInfo}. Timestamp: {timestamp}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request");
+            }
+            finally
+            {
+                var responseInfo = $"Response returned from \"{HttpContext.Request.Path}\" with status code {Response.StatusCode}. Timestamp: {timestamp}";
+                _logger.LogInformation(responseInfo);
+            }
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult> Update(string tableName, [FromRoute] string id, [FromBody] IDictionary<string, object> values)
         {
-            await _service.UpdateAsync(tableName, id, values);
-            var newItem = await _service.GetByIdAsync(tableName, id);
-            return Ok(newItem);
+            var timestamp = DateTimeOffset.UtcNow;
+            var valuesDict = values.ToDictionary(pair => pair.Key, pair => pair.Value?.ToString());
+
+            var requestInfo = $"PUT request to \"{HttpContext.Request.Path}\" from \"{HttpContext.Connection.RemoteIpAddress}\" by user \"{User.Identity.Name ?? "unknown"}\". Timestamp: {timestamp}";
+            _logger.LogInformation(requestInfo);
+
+            try
+            {
+                _logger.LogInformation("Updating entity with id \"{id}\" in \"{tableName}\": \"{values}\". Timestamp: {timestamp}", id, tableName, JsonConvert.SerializeObject(valuesDict), timestamp);
+                await _service.UpdateAsync(tableName, id, values);
+
+                var updatedItem = await _service.GetByIdAsync(tableName, id);
+
+                if (updatedItem == null)
+                {
+                    _logger.LogInformation($"No entity found with id \"{id}\" in \"{tableName}\". Request: {requestInfo}. Timestamp: {timestamp}");
+                    return NotFound();
+                }
+
+                _logger.LogInformation("Updated entity with id \"{id}\" in \"{tableName}\". Timestamp: {timestamp}", id, tableName, timestamp);
+                return Ok(updatedItem);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error while processing request: {requestInfo}. Timestamp: {timestamp}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request");
+            }
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> Delete(string tableName, string id)
         {
-            await _service.DeleteAsync(tableName, id);
+            var timestamp = DateTimeOffset.UtcNow;
+            var requestInfo = $"GET request to {HttpContext.Request.Path} from {HttpContext.Connection.RemoteIpAddress} by user {User.Identity.Name ?? "unknown."}. Timestamp: {timestamp}";
+            _logger.LogInformation(requestInfo);
+
+            _logger.LogInformation("Deleting entity with id {id} from {tableName}. Timestamp: {timestamp}", id, tableName, timestamp);
+
+            try
+            {
+                await _service.DeleteAsync(tableName, id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error while processing request: {requestInfo} - {ex}. Timestamp: {timestamp}");
+                throw;
+            }
+
+            var deletedItem = await _service.GetByIdAsync(tableName, id);
+
+            if (deletedItem != null)
+            {
+                _logger.LogInformation("Failed to delete entity with id {id} from {tableName}. Timestamp: {timestamp}", id, tableName, timestamp);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            _logger.LogInformation("Deleted entity with id {id} from {tableName}. Timestamp: {timestamp}", id, tableName, timestamp);
             return Ok();
         }
     }
--- GenericTableAPI/Program.cs
+++ GenericTableAPI/Program.cs
@@ -1,7 +1,7 @@
-using Microsoft.Extensions.Configuration;
+
 using GenericTableAPI.Repositories;
 using GenericTableAPI.Services;
-using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 
 namespace GenericTableAPI
 {
@@ -11,15 +11,27 @@ namespace GenericTableAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddSingleton(new DapperRepository(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddScoped<DapperService>();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSerilog();
 
             var app = builder.Build();
 
