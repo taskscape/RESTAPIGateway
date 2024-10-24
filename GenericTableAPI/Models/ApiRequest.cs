6d7cf5b9981ecfede43c63e266d72718679c225d
Kasper Ziębicki <kasper.ziebicki@taskscape.com>
2024-10-24 14:42:32 +0200

    WIP: foreach feature implementation

--- GenericTableAPI/Models/ApiRequest.cs
+++ GenericTableAPI/Models/ApiRequest.cs
@@ -4,6 +4,17 @@ public class ApiRequest
 {
     public string? Method { get; set; }
     public string? Endpoint { get; set; }
+    public string? Foreach {
+        set
+        {
+            _foreach = value?.Trim().TrimStart('{').TrimEnd('}');
+        }
+        get
+        {
+            return _foreach;
+        }
+    }
+    private string? _foreach;
     public Dictionary<string, string>? Parameters { get; set; }
     public Dictionary<string, string>? Returns { get; set; }
 }
