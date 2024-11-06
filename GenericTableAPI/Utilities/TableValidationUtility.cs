d1185d31990eb3a9558503a6f518f688fc2498e5
Kasper Ziębicki <k.ziebicki40@gmail.com>
2024-11-06 06:15:22 +0100

    Username based authentication (#15)
    * Added username based authentication
    * Updated documentation
    ---------
    
    Co-authored-by: Kasper Ziębicki <kasper.ziebicki@taskscape.com>

--- GenericTableAPI/Utilities/TableValidationUtility.cs
+++ GenericTableAPI/Utilities/TableValidationUtility.cs
@@ -1,8 +1,11 @@
-﻿namespace GenericTableAPI.Utilities
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace GenericTableAPI.Utilities
 {
     public static class TableValidationUtility
     {
-        public static bool ValidTablePermission(IConfiguration configuration, string tableName, string permission, string[]? Roles = null)
+        public static bool ValidTablePermission(IConfiguration configuration, string tableName, string permission, ClaimsPrincipal? User = null)
         {
             if (!configuration.GetSection("Database").Exists()) return true;
             IConfigurationSection tableSection = configuration.GetSection("Database:Tables:" + tableName);
@@ -17,9 +20,50 @@
             List<string>? rolePermissions = roleSection.Get<List<string>>();
             if (rolePermissions?.FirstOrDefault() == "*") return true;
 
-            if (Roles?.Length > 0)
-                return rolePermissions.Intersect(Roles).Any();
+            if (User != null)
+            {
+                var roles = GetUserRoles(User);
+                if(roles?.Length > 0)
+                    return rolePermissions
+                        .Select(x => x.StartsWith("rolename:") || x.StartsWith("username:") ? x : $"rolename:{x}")
+                        .Intersect(roles)
+                        .Any();
+            }
+
             return false;
+        }
+
+        public static string[]? GetUserRoles(ClaimsPrincipal User)
+        {
+            List<string> userClaims;
+
+            if (User.Identity is WindowsIdentity windowsIdentity && windowsIdentity.Groups != null)
+            {
+                //Windows user groups
+                userClaims = windowsIdentity.Groups
+                .Select(x => x.Translate(typeof(NTAccount)).Value)
+                //.Select(x => x.Substring(x.IndexOf('\\') + 1)) //Ignore "DOMAIN\"
+                .Select(x => $"rolename:{x}")
+                .ToList();
+
+                string username = User.Identity.Name;
+                if (username != null)
+                    //userClaims.Add($"username:{username.Substring(username.IndexOf('\\') + 1)}");//Ignore "DOMAIN\"
+                    userClaims.Add($"username:{username}");//Dont ignore "DOMAIN\"
+            }
+            else
+            {
+                //Claims Roles
+                userClaims = User.Claims
+                    .Where(c => c.Type == ClaimTypes.Role)
+                    .Select(c => $"rolename:{c.Value}")
+                    .ToList();
+
+                if (User.Identity?.Name != null)
+                    userClaims.Add($"username:{User.Identity.Name}");
+            }
+
+            return userClaims.ToArray();
         }
     }
 }
