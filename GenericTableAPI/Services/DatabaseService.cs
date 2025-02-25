0170d190909f600049388456605ef28c8c0ace69
Kasper Ziębicki <kasper.ziebicki@taskscape.com>
2025-02-25 14:45:38 +0100

    Implemented Retry Policy

--- GenericTableAPI/Services/DatabaseService.cs
+++ GenericTableAPI/Services/DatabaseService.cs
@@ -1,46 +1,76 @@
 ﻿using GenericTableAPI.Models;
 using GenericTableAPI.Repositories;
-namespace GenericTableAPI.Services;
+using Polly;
+using Polly.Retry;
 
-public class DatabaseService(DapperRepository repository)
+namespace GenericTableAPI.Services
 {
-    public Task<IEnumerable<dynamic>?> GetAllAsync(string tableName, string? where, string? orderBy, int? limit)
+    public class DatabaseService
     {
-        return repository.GetAllAsync(tableName, where, orderBy, limit);
-    }
+        private readonly DapperRepository _repository;
+        private readonly ILogger<DatabaseService> _logger;
+        private readonly AsyncRetryPolicy _retryPolicy;
 
-    public Task<dynamic?> GetByIdAsync(string tableName, string id, string? columnName = "")
-    {
-        return repository.GetByIdAsync(tableName, id, columnName);
-    }
+        public DatabaseService(DapperRepository repository, ILogger<DatabaseService> logger, IConfiguration configuration)
+        {
+            int retryCount;
+            if (!int.TryParse(configuration.GetSection("RetryPolicy:RetryCount").Value, out retryCount))
+                retryCount = 0;
 
-    public Task<object?> AddAsync(string tableName, IDictionary<string, object?> values, string? columnName = "")
-    {
-        return repository.AddAsync(tableName, values, columnName);
-    }
+            int retryWait;
+            if (!int.TryParse(configuration.GetSection("RetryPolicy:WaitDurationSeconds").Value, out retryWait))
+                retryWait = 3;
 
-    public Task<bool> UpdateAsync(string tableName, string id, IDictionary<string, object?> values, List<object> columns, string? columnName = "")
-    {
-        return repository.UpdateAsync(tableName, id, values, columns, columnName);
-    }
-    
-    public Task<bool> PatchAsync(string tableName, string id, IDictionary<string, object?> values, string? columnName = "")
-    {
-        return repository.PatchAsync(tableName, id, values, columnName);
-    }
+            _repository = repository;
+            _logger = logger;
 
-    public Task<bool> DeleteAsync(string tableName, string id, string? columnName = "")
-    {
-        return repository.DeleteAsync(tableName, id, columnName);
-    }
-    
-    public Task<List<object>?> ExecuteAsync(string procedureName, IEnumerable<StoredProcedureParameter?> values)
-    {
-        return repository.ExecuteAsync(procedureName, values);
-    }
-    
-    public Task<List<object>?> GetColumnsAsync(string tableName)
-    {
-        return repository.GetColumnsAsync(tableName);
+            _retryPolicy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(retryCount, retryAttempt => TimeSpan.FromSeconds(retryWait),
+                    (exception, timeSpan, retryCount, context) =>
+                    {
+                        _logger.LogWarning("Retry {retryCount} due to exception: {exceptionMessage}. Waiting for {timeSpanTotalSeconds} seconds.", retryCount, exception.Message, timeSpan.TotalSeconds);
+                    });
+        }
+
+        public async Task<IEnumerable<dynamic>?> GetAllAsync(string tableName, string? where, string? orderBy, int? limit)
+        {
+            return await _retryPolicy.ExecuteAsync(() => _repository.GetAllAsync(tableName, where, orderBy, limit));
+        }
+
+        public async Task<dynamic?> GetByIdAsync(string tableName, string id, string? columnName = "")
+        {
+            return await _retryPolicy.ExecuteAsync(() => _repository.GetByIdAsync(tableName, id, columnName));
+        }
+
+        public async Task<object?> AddAsync(string tableName, IDictionary<string, object?> values, string? columnName = "")
+        {
+            return await _retryPolicy.ExecuteAsync(() => _repository.AddAsync(tableName, values, columnName));
+        }
+
+        public async Task<bool> UpdateAsync(string tableName, string id, IDictionary<string, object?> values, List<object> columns, string? columnName = "")
+        {
+            return await _retryPolicy.ExecuteAsync(() => _repository.UpdateAsync(tableName, id, values, columns, columnName));
+        }
+
+        public async Task<bool> PatchAsync(string tableName, string id, IDictionary<string, object?> values, string? columnName = "")
+        {
+            return await _retryPolicy.ExecuteAsync(() => _repository.PatchAsync(tableName, id, values, columnName));
+        }
+
+        public async Task<bool> DeleteAsync(string tableName, string id, string? columnName = "")
+        {
+            return await _retryPolicy.ExecuteAsync(() => _repository.DeleteAsync(tableName, id, columnName));
+        }
+
+        public async Task<List<object>?> ExecuteAsync(string procedureName, IEnumerable<StoredProcedureParameter?> values)
+        {
+            return await _retryPolicy.ExecuteAsync(() => _repository.ExecuteAsync(procedureName, values));
+        }
+
+        public async Task<List<object>?> GetColumnsAsync(string tableName)
+        {
+            return await _retryPolicy.ExecuteAsync(() => _repository.GetColumnsAsync(tableName));
+        }
     }
 }
