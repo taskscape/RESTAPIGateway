6d266ebb60b1593716819cfaff6711b8d9adbc9b
Kasper Ziębicki <kasper.ziebicki@taskscape.com>
2025-03-28 10:25:20 +0100

    WIP: Improved Response error in Composite endpoint

--- GenericTableAPI/Exceptions/ResponseException.cs
+++ GenericTableAPI/Exceptions/ResponseException.cs
@@ -2,11 +2,16 @@
 {
     public class ResponseException : Exception
     {
-        public int StatusCode { get; set; }
+        public int StatusCode { get; }
+        public string? ErrorCode { get; }
+        public object? AdditionalData { get; }
 
-        public ResponseException(int statusCode, string? message = null) : base(message)
+        public ResponseException(int statusCode, string? message = null, string? errorCode = null, object? additionalData = null)
+            : base(message)
         {
             StatusCode = statusCode;
+            ErrorCode = errorCode;
+            AdditionalData = additionalData;
         }
     }
 }
